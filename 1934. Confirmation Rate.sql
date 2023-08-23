c97dfcfe181f145c9d2487afe55bbeda3cabc205
Pavitra Mohanarajan <114989541+pm2264@users.noreply.github.com>
2023-08-23 12:21:06 +0530

    Create 1934. Confirmation Rate.sql

---
+++ 1934. Confirmation Rate.sql
@@ -0,0 +1,5 @@
+select Signups.user_id, ifnull(round(sum(action = 'confirmed') / count(*), 2), 0.00) as confirmation_rate
+    from Signups
+    left join Confirmations
+    on Signups.user_id = Confirmations.user_id
+    group by Signups.user_id;
