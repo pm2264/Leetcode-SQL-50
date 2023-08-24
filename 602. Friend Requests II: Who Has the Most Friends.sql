085c916904ba6c8b564b80be87d1747ba5ac85bb
Pavitra Mohanarajan <114989541+pm2264@users.noreply.github.com>
2023-08-24 09:00:02 +0530

    Create 602. Friend Requests II: Who Has the Most Friends.sql

---
+++ 602. Friend Requests II: Who Has the Most Friends.sql
@@ -0,0 +1,13 @@
+with cte as 
+(select requester_id as id
+from requestaccepted
+union all
+select accepter_id
+from requestaccepted
+)
+
+select id, count(*) as num
+from cte
+group by id
+order by num desc
+limit 1;
